c34e8723a38e8e6aa6ba0d48b559feebe6be7979
shuvo-me <shuvo.js.dev@gmail.com>
2024-04-26 10:47:47 +0600

    add: ts in navlinks array

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,35 +3,46 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import React from 'react';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+type Link = {
+  name: string;
+  href: string;
+  icon: React.ReactNode;
+};
+const links: Array<Link> = [
+  { name: 'Home', href: '/dashboard', icon: <HomeIcon className="w-6" /> },
   {
     name: 'Invoices',
     href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    icon: <DocumentDuplicateIcon className="w-6" />,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  {
+    name: 'Customers',
+    href: '/dashboard/customers',
+    icon: <UserGroupIcon className="w-6" />,
+  },
 ];
 
-export default function NavLinks() {
+const NavLinks = () => {
   return (
     <>
       {links.map((link) => {
-        const LinkIcon = link.icon;
         return (
           <a
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
-            <LinkIcon className="w-6" />
+            {link.icon}
             <p className="hidden md:block">{link.name}</p>
           </a>
         );
       })}
     </>
   );
-}
+};
+
+export default NavLinks;
