8e0f9b3c8d0120c791573fd1c43f0b52efd6410c
shuvo-me <shuvo.js.dev@gmail.com>
2024-04-27 17:34:37 +0600

    remove: console.log

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,8 +8,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathName = usePathname();
   const { replace } = useRouter();
   const handleChange = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
