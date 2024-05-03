0a223f347bc7e7a6edb5a443b80d9f8563c7ec32
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-03 12:25:55 +0600

    add: root layout mata data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
-import '@/app/ui/global.css'
+import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metaData: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+};
 export default function RootLayout({
   children,
 }: {
