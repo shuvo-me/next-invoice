0a22326d019d4d46b1e98e20c3f64055e6644410
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-03 11:19:14 +0600

    create: authenticate function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 import { State } from './definitions';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -19,6 +21,25 @@ const FormSchema = z.object({
   }),
   date: z.string(),
 });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
+}
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
