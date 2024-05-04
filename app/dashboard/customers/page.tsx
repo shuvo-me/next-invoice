125f1ebb37585b50324f0c930a737be181399d24
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-04 13:20:44 +0600

    remove: unused code from customer table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,7 @@
-import { fetchCustomerPages, fetchFilteredCustomers } from '@/app/lib/data';
-import { lusitana } from '@/app/ui/fonts';
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
 import { Metadata } from 'next';
-import { Suspense } from 'react';
 import Table from '@/app/ui/customers/table';
-import { CreateCustomer } from '@/app/ui/customers/buttons';
 export const metadata: Metadata = {
   title: 'Customers',
 };
